3487f5bcff69e9dd28155f48e0c2a79939b3ce4f
Peter Malak <pmalak14@gmail.com>
2026-03-20 23:11:11 +0200

    testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -56,9 +56,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+        // if (app()->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
 
         $appUrl = config('app.url');
         $pathFromUrl = $appUrl ? parse_url($appUrl, PHP_URL_PATH) : null;
